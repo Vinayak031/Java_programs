8f2f208859594932e101c2e4a71e1b1845b66be9
Vinayak031 <121757013+Vinayak031@users.noreply.github.com>
2023-01-02 23:44:46 +0530

    this program will generates number b/w 1 to 1000

---
+++ _3randomNUm.java
@@ -0,0 +1,6 @@
+public class _3randomNUm {
+    public static void main(String[] args) {
+        // Generating random number using math.random range b/w 1000
+        System.out.println((int) (Math.random() * 1000));
+    }
+}
