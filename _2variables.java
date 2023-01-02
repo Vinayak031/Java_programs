8a1a9c0429642e3dcf3db326710f755522f6c08e
Vinayak031 <121757013+Vinayak031@users.noreply.github.com>
2023-01-02 23:43:26 +0530

    those are the variables can used in java

---
+++ _2variables.java
@@ -0,0 +1,56 @@
+
+public class _2variables {
+    public static void main(String[] args) {
+        // -2^n-1 to (2^n-1)-1
+
+        // boolean is a data type and it is capable of storing 0 to 255 values
+        // it is generally stores 0(or)1 [or] true (or) false
+        boolean q1 = true;
+        System.out.println(q1);
+
+        // Byte is a integer data which is capable of storing 8 bit(-128 to 127) int
+        // values
+        byte a = 10;
+        System.out.println(a);
+
+        // short is also an integer data type which is capable of storing 16 bit int
+        // values
+        short a1 = 200;
+        System.out.println(a1);
+
+        // int is a data type which is cpable of storing 32 bit int values
+        int a2 = 300;
+        System.out.println(a2);
+
+        // long is a integer data type which is capable of storing 64 bit int values
+        long a3 = 400;
+        System.out.println(a3);
+
+        // float is data type which is capable of storing 32 bit floating point values
+        float f1 = 1.2f;// float is represented ending with by letter f (or) F in java
+        System.out.println(f1);
+
+        // double is data type which is capable of storing 64 bit floating point values
+        // double f2=2.63633d; double is represented ending with by letter d (or) D in
+        // java
+        // (or)
+        double f2 = 2.63633;
+        // every floating point values are by default double
+        System.out.println(f2);
+
+        // char is data type which is capable of storing only one character
+        // it is capable of storing 16 bit char data
+        char c1 = '@';
+        System.out.println(c1);
+
+        // String are used to store a word (or) name
+        String s1 = "vinayak";
+        System.out.println(s1);
+        // var is data type which is used to store value of any data type Dynamically
+        var v = 2005;
+        System.out.println(v);
+
+        var v1 = "vinayak";
+        System.out.println(v1);
+    }
+}
