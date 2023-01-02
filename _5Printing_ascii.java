f5a4ca65f90c1c19f8614ea6f788e27f7e971b04
Vinayak031 <121757013+Vinayak031@users.noreply.github.com>
2023-01-02 23:46:43 +0530

    this program will print ASCII values of entered characters

---
+++ _5Printing_ascii.java
@@ -0,0 +1,7 @@
+public class _5Printing_ascii {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 150; i++) {
+            System.out.println(i + " " + (char) i);
+        }
+    }
+}
